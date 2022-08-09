c908ab57be380afe97fb25d4e46be706dc58ad75
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-08-09 21:00:06 +0300

    docs(tcp): add documentation for the "TcpServer" class

--- SimpleTcpServer/TcpServer.cs
+++ SimpleTcpServer/TcpServer.cs
@@ -3,6 +3,9 @@ using System.Net.Sockets;
 
 namespace SimpleTcpServer;
 
+/// <summary>
+/// Runs a server that listens for requests from TCP network clients.
+/// </summary>
 public sealed class TcpServer
 {
     private TcpListener? _server;
@@ -10,10 +13,19 @@ public sealed class TcpServer
     private bool _serverStarted;
     private Exception? _stopException;
 
+    /// <summary>
+    /// An IPAddress that represents the local IP address.
+    /// </summary>
     public IPAddress IpAddress { get; }
 
+    /// <summary>
+    /// The port on which to listen for requests.
+    /// </summary>
     public int Port { get; }
 
+    /// <summary>
+    /// The size of the byte array used as a buffer to store the request.
+    /// </summary>
     public int RequestBufferSize
     {
         get => _requestBufferSize;
@@ -26,14 +38,32 @@ public sealed class TcpServer
         }
     }
 
+    /// <summary>
+    /// The method used to handle a request.
+    /// </summary>
     public Func<IEnumerable<byte>, IEnumerable<byte>>? RequestHandler { get; set; }
 
+    /// <summary>
+    /// Occurs when the server starts.
+    /// </summary>
     public event EventHandler? ServerStart;
 
+    /// <summary>
+    /// Occurs when the server is stopped.
+    /// </summary>
     public event EventHandler<Exception?>? ServerStop;
 
+    /// <summary>
+    /// Initializes a new instance of the TcpServer class that runs a TCP server
+    /// which listens for requests on the specified local IP address and port number.
+    /// </summary>
+    /// <param name="ipAddress">An IPAddress that represents the local IP address.</param>
+    /// <param name="port">The port on which to listen for requests.</param>
     public TcpServer(IPAddress ipAddress, int port) => (IpAddress, Port) = (ipAddress, port);
 
+    /// <summary>
+    /// Starts the server.
+    /// </summary>
     public void Start()
     {
         if (_serverStarted is true)
@@ -63,6 +93,9 @@ public sealed class TcpServer
         AcceptConnection();
     }
 
+    /// <summary>
+    /// Stops the server.
+    /// </summary>
     public void Stop()
     {
         if (_serverStarted is false)
