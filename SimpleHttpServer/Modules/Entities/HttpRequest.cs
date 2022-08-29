00eb90788bbc0914e68c91e30f565f3ad4155688
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-08-29 19:11:49 +0300

    docs(http): add documentation for the HTTP server entities

--- SimpleHttpServer/Modules/Entities/HttpRequest.cs
+++ SimpleHttpServer/Modules/Entities/HttpRequest.cs
@@ -2,20 +2,50 @@
 
 namespace SimpleHttpServer.Modules.Entities;
 
+/// <summary>
+/// Represents an HTTP request.
+/// </summary>
 public sealed class HttpRequest
 {
+    /// <summary>
+    /// Method of an HTTP request.
+    /// </summary>
     public HttpRequestMethod Method { get; }
 
+    /// <summary>
+    /// Target of an HTTP request.
+    /// </summary>
     public string Target { get; }
 
+    /// <summary>
+    /// Query parameters of an HTTP request.
+    /// </summary>
     public IEnumerable<HttpRequestQueryParameter> QueryParameters { get; }
 
+    /// <summary>
+    /// Protocol version of an HTTP request.
+    /// </summary>
     public string ProtocolVersion { get; }
 
+    /// <summary>
+    /// Headers of an HTTP request.
+    /// </summary>
     public IEnumerable<HttpHeader> Headers { get; }
 
+    /// <summary>
+    /// Body of an HTTP request.
+    /// </summary>
     public string Body { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the HttpRequest class.
+    /// </summary>
+    /// <param name="method">Method of an HTTP request.</param>
+    /// <param name="target">Target of an HTTP request.</param>
+    /// <param name="queryParameters">Query parameters of an HTTP request.</param>
+    /// <param name="protocolVersion">Protocol version of an HTTP request.</param>
+    /// <param name="headers">Headers of an HTTP request.</param>
+    /// <param name="body">Body of an HTTP request.</param>
     public HttpRequest(HttpRequestMethod method, string target, IEnumerable<HttpRequestQueryParameter> queryParameters,
         string protocolVersion, IEnumerable<HttpHeader> headers, string body) =>
         (Method, Target, QueryParameters, ProtocolVersion, Headers, Body) =
--- SimpleHttpServer/Modules/Entities/HttpResponse.cs
+++ SimpleHttpServer/Modules/Entities/HttpResponse.cs
@@ -2,16 +2,38 @@
 
 namespace SimpleHttpServer.Modules.Entities;
 
+/// <summary>
+/// Represents an HTTP response.
+/// </summary>
 public sealed class HttpResponse
 {
+    /// <summary>
+    /// Protocol version of an HTTP response.
+    /// </summary>
     public string ProtocolVersion { get; set; }
 
+    /// <summary>
+    /// Status of an HTTP response.
+    /// </summary>
     public HttpResponseStatus Status { get; set; }
 
+    /// <summary>
+    /// Headers of an HTTP response.
+    /// </summary>
     public IEnumerable<HttpHeader> Headers { get; set; }
 
+    /// <summary>
+    /// Body of an HTTP response.
+    /// </summary>
     public string Body { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the HttpResponse class.
+    /// </summary>
+    /// <param name="protocolVersion">Protocol version of an HTTP response.</param>
+    /// <param name="status">Status of an HTTP response.</param>
+    /// <param name="headers">Headers of an HTTP response.</param>
+    /// <param name="body">Body of an HTTP response.</param>
     public HttpResponse(string protocolVersion, HttpResponseStatus status, IEnumerable<HttpHeader> headers, string body) =>
         (ProtocolVersion, Status, Headers, Body) = (protocolVersion, status, headers, body);
 }
