7fe1ab1748cffc384f325bba7eae4683e58f3dd7
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-12-05 19:48:01 +0300

    feat(tcp): add "TcpRequest" and "TcpResponse" entities

---
+++ SimpleTcpServer/Modules/Entities/TcpRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleTcpServer.Modules.Entities;
+
+/// <summary>
+/// Represents a TCP request.
+/// </summary>
+public sealed class TcpRequest
+{
+    /// <summary>
+    /// Gets the ID of the connection through which the request came.
+    /// </summary>
+    public Guid ConnectionId { get; }
+
+    /// <summary>
+    /// Gets the body of the TCP request.
+    /// </summary>
+    public IEnumerable<byte> Body { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the TcpRequest class.
+    /// </summary>
+    /// <param name="connectionId">The connection through which the request came.</param>
+    /// <param name="body">The body of the TCP request.</param>
+    public TcpRequest(Guid connectionId, IEnumerable<byte> body) =>
+        (ConnectionId, Body) = (connectionId, body);
+}
---
+++ SimpleTcpServer/Modules/Entities/TcpResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleTcpServer.Modules.Entities;
+
+/// <summary>
+/// Represents a TCP response.
+/// </summary>
+public sealed class TcpResponse
+{
+    /// <summary>
+    /// Gets or sets a value determining whether the connection should be maintained after the request has been handled.
+    /// </summary>
+    public bool KeepConnectionAlive { get; set; }
+
+    /// <summary>
+    /// Gets or sets the body of the TCP response.
+    /// </summary>
+    public IEnumerable<byte> Body { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the TcpResponse class.
+    /// </summary>
+    /// <param name="body">The body of the TCP response.</param>
+    public TcpResponse(IEnumerable<byte> body) =>
+        Body = body;
+}
