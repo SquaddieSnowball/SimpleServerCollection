48411c767f4d138bd2cbbefb991eb920e7553716
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-12-29 22:28:10 +0300

    test(http): add unit tests for "Simple HTTP server"

---
+++ SimpleHttpServer.Tests/Helpers/HttpRequestParserTests.cs
@@ -0,0 +1,67 @@
+﻿using SimpleHttpServer.Modules.Helpers;
+using System.Text;
+
+namespace SimpleHttpServer.Tests.Helpers;
+
+[TestClass]
+public sealed class HttpRequestParserTests
+{
+    [TestMethod]
+    public void ParseFromBytes_EmptyString_ThrowsArgumentException()
+    {
+        string request = string.Empty;
+        IEnumerable<byte> requestBytes = Encoding.ASCII.GetBytes(request);
+
+        void actual() => _ = HttpRequestParser.ParseFromBytes(requestBytes);
+
+        _ = Assert.ThrowsException<ArgumentException>(actual);
+    }
+
+    [TestMethod]
+    public void ParseFromBytes_InvalidStartLine_ThrowsArgumentException()
+    {
+        string request = "GET";
+        IEnumerable<byte> requestBytes = Encoding.ASCII.GetBytes(request);
+
+        void actual() => _ = HttpRequestParser.ParseFromBytes(requestBytes);
+
+        _ = Assert.ThrowsException<ArgumentException>(actual);
+    }
+
+    [TestMethod]
+    public void ParseFromBytes_IvalidTargetParameter_ThrowsArgumentException()
+    {
+        string request = "GET /?parameter HTTP/1.0";
+        IEnumerable<byte> requestBytes = Encoding.ASCII.GetBytes(request);
+
+        void actual() => _ = HttpRequestParser.ParseFromBytes(requestBytes);
+
+        _ = Assert.ThrowsException<ArgumentException>(actual);
+    }
+
+    [TestMethod]
+    public void ParseFromBytes_InvalidHeader_ThrowsArgumentException()
+    {
+        string request = "GET / HTTP/1.0\nHeader";
+        IEnumerable<byte> requestBytes = Encoding.ASCII.GetBytes(request);
+
+        void actual() => _ = HttpRequestParser.ParseFromBytes(requestBytes);
+
+        _ = Assert.ThrowsException<ArgumentException>(actual);
+    }
+
+    [TestMethod]
+    [DataRow("GET / HTTP/1.0")]
+    [DataRow("GET /?parameter=value HTTP/1.0")]
+    [DataRow("GET / HTTP/1.0\nHeader: value")]
+    [DataRow("GET / HTTP/1.0\n\nBody")]
+    [DataRow("GET /?parameter=value HTTP/1.0\nHeader: value\n\nBody")]
+    public void ParseFromBytes_MultipleRequests_DoesNotThrowAnException(string request)
+    {
+        IEnumerable<byte> requestBytes = Encoding.ASCII.GetBytes(request);
+
+        _ = HttpRequestParser.ParseFromBytes(requestBytes);
+
+        Assert.IsTrue(true);
+    }
+}
---
+++ SimpleHttpServer.Tests/Helpers/HttpResponseBuilderTests.cs
@@ -0,0 +1,48 @@
+﻿using SimpleHttpServer.Modules.Entities;
+using SimpleHttpServer.Modules.Entities.Components;
+using SimpleHttpServer.Modules.Helpers;
+using System.Text;
+
+namespace SimpleHttpServer.Tests.Helpers;
+
+[TestClass]
+public sealed class HttpResponseBuilderTests
+{
+    [TestMethod]
+    public void Build_SingleLineResponse_ReturnsCorrectBytes()
+    {
+        string textResponse = "HTTP/1.0 200 OK";
+        IEnumerable<byte> expected = Encoding.ASCII.GetBytes(textResponse);
+        HttpResponse httpResponse = new("1.0", HttpResponseStatus.OK, Enumerable.Empty<HttpHeader>());
+
+        IEnumerable<byte> actual = HttpResponseBuilder.Build(httpResponse);
+
+        CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+    }
+
+    [TestMethod]
+    public void Build_ResponseWithHeader_ReturnsCorrectBytes()
+    {
+        string textResponse = "HTTP/1.0 200 OK\nHeader: value";
+        IEnumerable<byte> expected = Encoding.ASCII.GetBytes(textResponse);
+        HttpResponse httpResponse = new("1.0", HttpResponseStatus.OK, new HttpHeader[]
+            { new HttpHeader(HttpHeaderGroup.Response, "Header", "value") });
+
+        IEnumerable<byte> actual = HttpResponseBuilder.Build(httpResponse);
+
+        CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+    }
+
+    [TestMethod]
+    public void Build_ResponseWithHeaderAndBody_ReturnsCorrectBytes()
+    {
+        string textResponse = "HTTP/1.0 200 OK\nHeader: value\n\nBody";
+        IEnumerable<byte> expected = Encoding.ASCII.GetBytes(textResponse);
+        HttpResponse httpResponse = new("1.0", HttpResponseStatus.OK, new HttpHeader[]
+            { new HttpHeader(HttpHeaderGroup.Response, "Header", "value") }, "Body");
+
+        IEnumerable<byte> actual = HttpResponseBuilder.Build(httpResponse);
+
+        CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+    }
+}
