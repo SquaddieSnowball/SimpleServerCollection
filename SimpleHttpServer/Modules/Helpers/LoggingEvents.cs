0924c556205aaff4a1ab285a9261297cd2174db6
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-09-28 16:08:57 +0300

    fix(http): update logging event names

--- SimpleHttpServer/Modules/Helpers/LoggingEvents.cs
+++ SimpleHttpServer/Modules/Helpers/LoggingEvents.cs
@@ -3,16 +3,16 @@
 namespace SimpleHttpServer.Modules.Helpers;
 
 /// <summary>
-/// Represents a type that contains logging events IDs.
+/// Represents a type containing logging event IDs.
 /// </summary>
 internal static class LoggingEvents
 {
-    internal static EventId ServerStart = new(1000, "Server started");
-    internal static EventId ServerStop = new(1001, "Server stopped");
-    internal static EventId ServerStopException = new(1002, "Server stopped with exception");
-    internal static EventId ConnectionOpen = new(2000, "Connection opened");
-    internal static EventId ConnectionClose = new(2001, "Connection closed");
-    internal static EventId RequestHandling = new(3000, "Request handled");
-    internal static EventId RequestHandlingFailBeforeConnection = new(3001, "Request handling failed before connection");
-    internal static EventId RequestHandlingFailAfterConnection = new(3002, "Request handling failed after connection");
+    public static EventId ServerStart = new(1000, "Server is running");
+    public static EventId ServerStop = new(1001, "Server stopped");
+    public static EventId ServerStopException = new(1002, "Server stopped with exception");
+    public static EventId ConnectionOpen = new(2000, "Connection is open");
+    public static EventId ConnectionClose = new(2001, "Connection closed");
+    public static EventId RequestHandling = new(3000, "Request handled");
+    public static EventId RequestHandlingFailBeforeConnection = new(3001, "Request handling failed (before connection)");
+    public static EventId RequestHandlingFailAfterConnection = new(3002, "Request handling failed (after connection)");
 }
