922c5620d58c1a258cc5a71111e0f9c72138c3d6
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-10-18 21:31:27 +0300

    docs(htpp): add documentation for enum values

--- SimpleHttpServer/Modules/Entities/Components/HttpHeaderGroup.cs
+++ SimpleHttpServer/Modules/Entities/Components/HttpHeaderGroup.cs
@@ -5,8 +5,23 @@
 /// </summary>
 public enum HttpHeaderGroup
 {
+    /// <summary>
+    /// Represents the "Request" HTTP header group.
+    /// </summary>
     Request,
+
+    /// <summary>
+    /// Represents the "Response" HTTP header group.
+    /// </summary>
     Response,
+
+    /// <summary>
+    /// Represents the "Representation" HTTP header group.
+    /// </summary>
     Representation,
+
+    /// <summary>
+    /// Represents the "Payload" HTTP header group.
+    /// </summary>
     Payload
 }
