3dc0cdfce583222cc0bc6774d5eae4cd51ab3019
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-12-17 22:01:25 +0300

    test(http): update tests to match changes in HTTP server

---
+++ test/unit/SimpleHttpServer.UnitTests/Services/HttpRequestParserTests.cs
@@ -0,0 +1,65 @@
+﻿using SimpleHttpServer.Services.Implementations;
+using System.Text;
+
+namespace SimpleHttpServer.UnitTests.Services;
+
+[TestClass]
+public sealed class HttpRequestParserTests
+{
+    [TestMethod]
+    public void Parse_InvalidStartLine_ThrowsArgumentException()
+    {
+        HttpRequestParser parser = CreateHttpRequestParser();
+        string requestString = "GET";
+        IEnumerable<byte> requestBytes = Encoding.UTF8.GetBytes(requestString);
+
+        void actual() => _ = parser.Parse(requestBytes);
+
+        _ = Assert.ThrowsException<ArgumentException>(actual);
+    }
+
+    [TestMethod]
+    public void Parse_IvalidQueryParameter_ThrowsArgumentException()
+    {
+        HttpRequestParser parser = CreateHttpRequestParser();
+        string requestString = "GET /?parameter HTTP/1.0";
+        IEnumerable<byte> requestBytes = Encoding.UTF8.GetBytes(requestString);
+
+        void actual() => _ = parser.Parse(requestBytes);
+
+        _ = Assert.ThrowsException<ArgumentException>(actual);
+    }
+
+    [TestMethod]
+    public void Parse_InvalidHeader_ThrowsArgumentException()
+    {
+        HttpRequestParser parser = CreateHttpRequestParser();
+        string requestString = "GET / HTTP/1.0\r\nHeader";
+        IEnumerable<byte> requestBytes = Encoding.UTF8.GetBytes(requestString);
+
+        void actual() => _ = parser.Parse(requestBytes);
+
+        _ = Assert.ThrowsException<ArgumentException>(actual);
+    }
+
+    [TestMethod]
+    [DataRow("GET / HTTP/1.0")]
+    [DataRow("GET /?parameter=value HTTP/1.0")]
+    [DataRow("GET / HTTP/1.0\r\nHeader: value")]
+    [DataRow("GET /?parameter=value HTTP/1.0\r\nHeader: value")]
+    [DataRow("GET / HTTP/1.0\r\n\r\nBody")]
+    [DataRow("GET /?parameter=value HTTP/1.0\r\n\r\nBody")]
+    [DataRow("GET / HTTP/1.0\r\nHeader: value\r\n\r\nBody")]
+    [DataRow("GET /?parameter=value HTTP/1.0\r\nHeader: value\r\n\r\nBody")]
+    public void Parse_ValidRequest_DoesNotThrowAnException(string requestString)
+    {
+        HttpRequestParser parser = CreateHttpRequestParser();
+        IEnumerable<byte> requestBytes = Encoding.UTF8.GetBytes(requestString);
+
+        _ = parser.Parse(requestBytes);
+
+        Assert.IsTrue(true);
+    }
+
+    private static HttpRequestParser CreateHttpRequestParser() => new();
+}
---
+++ test/unit/SimpleHttpServer.UnitTests/Services/HttpResponseBuilderTests.cs
@@ -0,0 +1,85 @@
+﻿using SimpleHttpServer.Entities;
+using SimpleHttpServer.Entities.Components;
+using SimpleHttpServer.Services.Implementations;
+using System.Text;
+
+namespace SimpleHttpServer.UnitTests.Services;
+
+[TestClass]
+public sealed class HttpResponseBuilderTests
+{
+    [TestMethod]
+    public void Build_SingleLineResponse_ReturnsCorrectBytes()
+    {
+        HttpResponseBuilder builder = CreateHttpResponseBuilder();
+        HttpRequest httpRequest = new(
+            Enumerable.Empty<byte>(),
+            HttpRequestMethod.GET,
+            "/",
+            Enumerable.Empty<HttpRequestQueryParameter>(),
+            "1.0",
+            Enumerable.Empty<HttpHeader>());
+        HttpResponse httpResponse = new(
+            HttpResponseStatus.OK,
+            Enumerable.Empty<HttpHeader>());
+        string responseString = "HTTP/1.0 200 OK";
+        IEnumerable<byte> expected = Encoding.UTF8.GetBytes(responseString);
+
+        IEnumerable<byte> actual = builder.Build(httpRequest, httpResponse);
+
+        CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+    }
+
+    [TestMethod]
+    public void Build_ResponseWithHeader_ReturnsCorrectBytes()
+    {
+        HttpResponseBuilder builder = CreateHttpResponseBuilder();
+        HttpRequest httpRequest = new(
+            Enumerable.Empty<byte>(),
+            HttpRequestMethod.GET,
+            "/",
+            Enumerable.Empty<HttpRequestQueryParameter>(),
+            "1.0",
+            Enumerable.Empty<HttpHeader>());
+        HttpResponse httpResponse = new(
+            HttpResponseStatus.OK,
+            new HttpHeader[]
+            {
+                new(HttpHeaderGroup.Response, "Header", "value")
+            });
+        string responseString = "HTTP/1.0 200 OK\r\nHeader: value";
+        IEnumerable<byte> expected = Encoding.UTF8.GetBytes(responseString);
+
+        IEnumerable<byte> actual = builder.Build(httpRequest, httpResponse);
+
+        CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+    }
+
+    [TestMethod]
+    public void Build_ResponseWithHeaderAndBody_ReturnsCorrectBytes()
+    {
+        HttpResponseBuilder builder = CreateHttpResponseBuilder();
+        HttpRequest httpRequest = new(
+            Enumerable.Empty<byte>(),
+            HttpRequestMethod.GET,
+            "/",
+            Enumerable.Empty<HttpRequestQueryParameter>(),
+            "1.0",
+            Enumerable.Empty<HttpHeader>());
+        HttpResponse httpResponse = new(
+            HttpResponseStatus.OK,
+            new HttpHeader[]
+            {
+                new(HttpHeaderGroup.Response, "Header", "value")
+            },
+            Encoding.UTF8.GetBytes("Body"));
+        string responseString = "HTTP/1.0 200 OK\r\nHeader: value\r\n\r\nBody";
+        IEnumerable<byte> expected = Encoding.UTF8.GetBytes(responseString);
+
+        IEnumerable<byte> actual = builder.Build(httpRequest, httpResponse);
+
+        CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+    }
+
+    private static HttpResponseBuilder CreateHttpResponseBuilder() => new();
+}
